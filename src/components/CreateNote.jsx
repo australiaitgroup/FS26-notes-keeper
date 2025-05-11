3b4a84bdb9644bd87949333f5da2b04e47c80e28
kzfly1 <qichenxu47@gmail.com>
2025-05-11 22:56:41 +1000

    feat: add note creation UI

---
+++ src/components/CreateNote.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
+import Fab from "@mui/material/Fab";
+import Note from "./Note";
+
+const CreateNote = ({ addNote }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [newNote, setNewNote] = useState({ title: "", content: "" });
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const submitNote = e => {
+    if (!newNote.title || !newNote.content) {
+      setErrorMessage("Please fill in all fields");
+    } else {
+      addNote({ ...newNote, id: new Date() });
+      setNewNote({ title: "", content: "" });
+      setIsExpanded(false);
+      setErrorMessage("");
+    }
+    e.preventDefault();
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setNewNote({ ...newNote, [name]: value });
+  };
+
+  return (
+    <div className="create-area">
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            className="title"
+            type="text"
+            name="title"
+            value={newNote.title}
+            onChange={handleChange}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          className="textarea"
+          name="content"
+          value={newNote.content}
+          onChange={handleChange}
+          onClick={() => setIsExpanded(true)}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
+        />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+      <p>{errorMessage}</p>
+    </div>
+  );
+};
+
+export default CreateNote;
