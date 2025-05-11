ff297df1dda58871d7dd2b98add7c5aee2b2b732
ShatterBloom <pamtatech@gmail.com>
2025-05-11 23:05:42 +1000

    Add note creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import CreateNote from "./components/CreateNote";
+import Note from "./components/Note";
+import { useState } from "react";
 
 function App() {
-
+  const [notes, setNotes] = useState([
+    { id: new Date(), title: "test title", content: "test contend" },
+  ]);
+  const deleteNote = (deletedItemId) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem) => {
+        return deletedItemId !== noteItem.id;
+      });
+    });
+  };
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
   return (
     <>
-     notes keeper
+      <Header />
+      <div>
+        <CreateNote addNote={addNote} />
+        {notes.map((noteItem, index) => (
+          <Note key={noteItem.id} noteItem={noteItem} deleteNote={deleteNote} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NoteIcon from "@mui/icons-material/Note";
+import "../App.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>
+        <NoteIcon /> Keeper
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
