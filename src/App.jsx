02690853eaacfd9494a30de265b236582bc0413c
sk5642 <111110544+sk5642@users.noreply.github.com>
2025-05-11 22:55:23 +1000

    Implement note-taking app with add and delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css'
+import { useState } from 'react'
+import Note from './component/Note'
+import Header from './component/Header'
+import CreateNote from './component/CreateNote'
+
 
 function App() {
+  const [notes, setNotes] = useState([{id: new Date(), title: 'test Title', content: 'test Content'}])
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, itemToDeleteId) => {
+        return itemToDeleteId !== id
+      })
+    })
+  }
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote])
+  }
 
   return (
     <>
-     notes keeper
+      <Header />
+      <CreateNote />
+      <CreateNote addNote={addNote} />
+      {notes.map((noteItem) => {
+        return (
+          <Note key={noteItem.id} noteItem={noteItem} deleteNote={deleteNote}/>
+        )
+      })}
+
+
     </>
   )
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import NoteIcon from '@mui/icons-material/Note';
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+}
