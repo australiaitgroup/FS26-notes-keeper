8a3645dcb9003d5c4030730ec32e2a340419eb03
hulin <piaohulin2007@gmail.com>
2025-05-11 22:55:00 +1000

    createNote/Header/Note.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import './App.css'
+import { useState } from 'react';
+import Note from './components/Note';
+import Header from './components/Header';
+import CreateNote from './components/CreateNote';
 
 function App() {
+  const [notes, setNotes] = useState([{id: new Date(), title: 'test tile', content: 'test content'}]);
+  const deleteNote = (itemToDeleteId) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem) => {
+        return itemToDeleteId !== noteItem.id
+      })
+    })
+  }
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote]) 
+  }
 
   return (
     <>
-     notes keeper
+    <Header/>
+    <CreateNote addNote={addNote}/>
+     {notes.map((noteItem,index)=>{
+      return (
+        <Note key={noteItem.id} noteItem = {noteItem} deleteNote = {deleteNote}/>
+      )
+     })}
     </>
   )
 }
