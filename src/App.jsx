60f8ec6218dac6dfe39bace5574ff9af98124c18
Leon Xia <huachi1990@hotmail.com>
2025-05-11 22:59:42 +1000

    Feat/overall layout & add/delete note

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Note from './components/Note';
+import Header from './components/Header';
+import CreateNote from './components/CreateNote';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((prev) => {
+        return prev.id !== id;
+      });
+    });
+  };
+
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
 
   return (
     <>
-     notes keeper
+      <Header />
+      <CreateNote addNote={addNote} />
+      {notes.map((noteItem) => {
+        return (
+          <Note key={noteItem.id} noteItem={noteItem} deleteNote={deleteNote} />
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
